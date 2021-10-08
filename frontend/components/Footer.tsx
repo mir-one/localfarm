df402968e02305b5d29f384eebf878441b4ba9bc
Roman Inozemtsev <inozemtsev_roman@mail.ru>
2021-10-08 17:44:35 +0300

    Update Footer.tsx

--- frontend/components/Footer.tsx
+++ frontend/components/Footer.tsx
@@ -3,7 +3,7 @@ import { Container } from "react-bootstrap";
 const Footer = (): JSX.Element => (
   <footer className="mb-3">
     <Container fluid>
-    Локальная Ферма 1.0.0. Сделоно с ♥ к Миру &copy; 2021 Локальная Ферма.
+    Локальная Ферма 1.0.0. Сделано с ♥ к Миру &copy; 2021 Локальная Ферма.
     </Container>
   </footer>
 );
